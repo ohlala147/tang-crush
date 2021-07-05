1e785e9016f3124991d239689111da39ef8e5598
ohlala147 <86961152+ohlala147@users.noreply.github.com>
2021-07-05 18:23:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,18 +2,18 @@ const CONFIG = {
     introTitle: 'Chào bạn Quỳnh !',
     introDesc: `Trái đất vốn lạ thường
     Mà sao Q cứ đi nhầm đường
-    Lạc vào tim L lẻ loi
+    Lạc vào tim tui lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng Q đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng Q đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Q lại loay hoay trong tâm trí anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnYes: 'Vẫn cứ là thích tui <33',
+    btnNo: 'Không, bạn à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Q lại loay hoay trong tâm trí tui <3',
+    btnReply: 'Gửi cho ông <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'tui biết mà 🥰.',
+    messDesc: 'mạnh mẽ lên nhé cô gái,có tui đây rồi đừng lo gì hết nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
