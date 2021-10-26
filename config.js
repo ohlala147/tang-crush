18ae4a5adce57b9bfb953d9fe2c81843301f4d8a
ohlala147 <86961152+ohlala147@users.noreply.github.com>
2021-10-26 17:48:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Chào bạn Song Nhi !',
+    introTitle: 'Chào bạn Thanh Phương !',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao Nhi cứ đi nhầm đường
-    Lạc vào tim tui lẻ loi
+    Mà sao Phương cứ đi nhầm đường
+    Lạc vào tim Long lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng Nhi đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng Nhi đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích tui <33',
+    title: 'Phải chăng Phương đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Phương đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anhhh <33',
     btnNo: 'Không, bạn à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Nhi lại loay hoay trong tâm trí tui <3',
-    btnReply: 'Gửi cho ông <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Phương lại loay hoay trong tâm trí anhhh <3',
+    btnReply: 'Gửi cho anhh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'tui biết mà 🥰.',
-    messDesc: 'Chúc cô gái của tui thi thật tốt nha,cố gắng hết mình,đậu đại học mong muốn nha,cố lên nha Song Nhi ơi.',
+    mess: 'anh biết mà 🥰.',
+    messDesc: 'Chúc cô gái của anh luôn nhận được những điều tốt đẹp trong cuộc sống nha,anh chỉ muốn nói là anh thương em rất nhiều,lỡ thương em rồi anh không rời khỏi em được đâu.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
